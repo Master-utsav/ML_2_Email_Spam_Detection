473d82faa4804adae641a0ff06cac848b70993f6
master-utsav <utsavjaiswal890@gmail.com>
2026-05-08 22:15:02 +0530

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,19 @@
 
 echo "Starting deployment..."
 
-# Go to project folder
-cd ~/Spam\ Email\ Detector/web || exit
+cd ./web || exit
 
 echo "Pulling latest changes from GitHub..."
 git pull origin main
 
 echo "Installing dependencies..."
-npm install
+npm ci
 
 echo "Building application..."
 npm run build
 
 echo "Restarting PM2 app..."
-
-# If app already exists, restart it
-pm2 restart mlp0-web || pm2 start npm --name "mlp0-web" -- start
+pm2 restart mlp0-web --update-env || pm2 start npm --name "mlp0-web" -- start
 
 echo "Saving PM2 process list..."
 pm2 save
